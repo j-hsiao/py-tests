34dfb34a8d8d3e46a033ff34a7f4d037413a8df7
Hsiao <Jason.Hsiao@acer.com>
2022-09-05 10:09:58 +0100

    change names to use pep503 normalized names (. become -)

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from jhsiao.namespace import make_ns, fdir
 
 make_ns('jhsiao', dir=fdir(__file__))
 setup(
-    name='jhsiao.tests',
+    name='jhsiao-tests',
     version='0.0.1',
     author='Jason Hsiao',
     author_email='oaishnosaj@gmail.com',
     description='register/run tests',
     packages=['jhsiao.tests'],
-    install_requires=['jhsiao.utils @ git+https://github.com/j-hsiao/py-utils.git']
+    install_requires=['jhsiao-utils @ git+https://github.com/j-hsiao/py-utils.git']
 )
